f69a9676bd4dcf22e78bef261866674d8fa0fff9
Giulio Zanchetta <zank@outlook.it>
2023-09-06 19:03:18 +0200

    Update astro.config.mjs
    - Changed the repository from "warpsi/astros" to "zankhq/epicfolio"
    - Updated the base URL to "https://epicfolio.zank.studio"
    - Added new fields for portfolio posts: title, image, snippet, publish date, category, and body

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,9 +19,9 @@ export default defineConfig({
 			config: {
 				backend: {
 					name: "github",
-					repo: "warpsi/astros",
+					repo: "zankhq/epicfolio",
 					branch: "main",
-					base_url: "https://astros-7h0.pages.dev",
+					base_url: "https://epicfolio.zank.studio",
 					auth_endpoint: "/api/auth",
 				},
 				media_folder: "public/images",
@@ -34,7 +34,60 @@ export default defineConfig({
 						folder: "src/content/portfolio",
 						create: true,
 						delete: true,
-						fields: [],
+						fields: [
+							{
+								name: "title",
+								widget: "string",
+								label: "Post Title",
+								i18n: true,
+							},
+							{
+								label: "Image",
+								name: "image",
+								widget: "object",
+								i18n: true,
+								fields: [
+									{
+										label: "Source",
+										name: "src",
+										widget: "image",
+										i18n: "duplicate",
+									},
+									{
+										label: "Alt Text",
+										name: "alt",
+										widget: "string",
+										i18n: true,
+									},
+								],
+							},
+							{
+								label: "Snippet",
+								name: "snippet",
+								widget: "text",
+								i18n: true,
+							},
+							{
+								label: "Publish Date",
+								name: "publishDate",
+								widget: "datetime",
+								format: "YYYY-MM-DD HH:mm",
+								i18n: "duplicate",
+							},
+							{
+								label: "Category",
+								name: "category",
+								widget: "select",
+								options: ["Tutorials", "News", "Reviews", "Frameworks"],
+								i18n: "duplicate",
+							},
+							{
+								name: "body",
+								widget: "markdown",
+								label: "Post Body",
+								i18n: true,
+							},
+						],
 					},
 				],
 			},
